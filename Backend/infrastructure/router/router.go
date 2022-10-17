dd71364d2a5cb1b913c4ec124bec0031371a86d6
Palm <chalanthorn.a@ku.th>
2022-10-17 13:45:25 +0700

    ⚡️ update cors to be middleware

--- Backend/infrastructure/router/router.go
+++ Backend/infrastructure/router/router.go
@@ -3,13 +3,14 @@ package router
 import (
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/controllers"
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/infrastructure/database"
-	"github.com/ChalanthornA/Gold-Inventory-Management-System/middleware"
+	"github.com/ChalanthornA/Gold-Inventory-Management-System/middlewares"
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/repositories"
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/usecases"
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes(r *gin.Engine){
+func SetupRoutes(r *gin.Engine) {
+	r.Use(middlewares.CORSMiddleware())
 	userRepository := repositories.NewUserRepository(database.DB)
 	userUseCase := usecases.NewUserUseCase(userRepository)
 	userController := controllers.NewUserController(userUseCase)
@@ -19,11 +20,11 @@ func SetupRoutes(r *gin.Engine){
 		auth.POST("/registeradmin", userController.RegisterAdmin)
 		auth.POST("/signin", userController.SignIn)
 
-		auth.Use(middleware.AuthorizeJWT())
+		auth.Use(middlewares.AuthorizeJWT())
 		auth.POST("/register", userController.Register)
 		auth.GET("/profile", userController.TestJWT)
 	}
-	
+
 	goldRepository := repositories.NewGoldRepository(database.DB)
 	goldUseCase := usecases.NewGoldUseCase(goldRepository)
 	goldController := controllers.NewGoldController(goldUseCase)
--- Backend/main.go
+++ Backend/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/infrastructure/database"
 	"github.com/ChalanthornA/Gold-Inventory-Management-System/infrastructure/router"
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,7 +11,6 @@ func main() {
 	database.DB = database.NewDb()
 	defer database.DB.Close()
 
-	r.Use(cors.Default())
 	router.SetupRoutes(r)
 
 	r.Run()
