64df080d2c4e994d9e2cc3f0e501503fb9b04905
Palm <chalanthorn.a@ku.th>
2023-01-17 19:46:40 +0700

    🐛 fix bug in updateGoldDetailSql

--- repositories/gold_detail_repository.go
+++ repositories/gold_detail_repository.go
@@ -170,9 +170,9 @@ func (gr *goldRepository) UpdateGoldDetail(goldDetail *models.GoldDetail) error{
 	updateGoldDetailSql := `
 		UPDATE gold_details
 		SET code = $1, type = $2, detail = $3, weight = $4, gold_percent = $5, gold_smith_fee = $6, picture = $7
-		WHERE gold_detail_id = $7;
+		WHERE gold_detail_id = $8;
 	`
-	_, err := gr.db.Exec(gr.ctx, updateGoldDetailSql, goldDetail.Code, goldDetail.Type, goldDetail.Detail, goldDetail.Weight, goldDetail.GoldPercent, goldDetail.GoldSmithFee, goldDetail.GoldDetailID, goldDetail.Picture)
+	_, err := gr.db.Exec(gr.ctx, updateGoldDetailSql, goldDetail.Code, goldDetail.Type, goldDetail.Detail, goldDetail.Weight, goldDetail.GoldPercent, goldDetail.GoldSmithFee, goldDetail.Picture, goldDetail.GoldDetailID)
 	return err
 }
 
