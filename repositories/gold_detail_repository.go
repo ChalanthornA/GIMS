2991b53ccf8b63977d777b429623b30d6cbc07f5
Palm <chalanthorn.a@ku.th>
2023-01-17 18:44:11 +0700

    ⚡️ add picture in update gold detail path

--- repositories/gold_detail_repository.go
+++ repositories/gold_detail_repository.go
@@ -169,10 +169,10 @@ func (gr *goldRepository) QueryGoldDetailByDetail(g *models.GoldDetail) ([]model
 func (gr *goldRepository) UpdateGoldDetail(goldDetail *models.GoldDetail) error{
 	updateGoldDetailSql := `
 		UPDATE gold_details
-		SET code = $1, type = $2, detail = $3, weight = $4, gold_percent = $5, gold_smith_fee = $6
+		SET code = $1, type = $2, detail = $3, weight = $4, gold_percent = $5, gold_smith_fee = $6, picture = $7
 		WHERE gold_detail_id = $7;
 	`
-	_, err := gr.db.Exec(gr.ctx, updateGoldDetailSql, goldDetail.Code, goldDetail.Type, goldDetail.Detail, goldDetail.Weight, goldDetail.GoldPercent, goldDetail.GoldSmithFee, goldDetail.GoldDetailID)
+	_, err := gr.db.Exec(gr.ctx, updateGoldDetailSql, goldDetail.Code, goldDetail.Type, goldDetail.Detail, goldDetail.Weight, goldDetail.GoldPercent, goldDetail.GoldSmithFee, goldDetail.GoldDetailID, goldDetail.Picture)
 	return err
 }
 
